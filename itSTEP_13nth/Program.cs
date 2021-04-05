62a96af56f1c81bab20399e45ebe713599c61d31
Issabek <180107287@stu.sdu.edu.kz>
2021-04-05 14:51:50 +0600

    done

---
+++ itSTEP_13nth/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace itSTEP_13nth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Temperature temp = new Temperature();
+            try
+            {
+                temp.showTemp();
+            }
+            catch (IsZeroException e)
+            {
+                Console.WriteLine("IsZeroException: {0}", e.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+    public class IsZeroException : Exception
+    {
+        public IsZeroException(string message) : base(message)
+        {
+        }
+        public IsZeroException()
+        {
+        }
+    }
+    public class Temperature
+    {
+        int temperature = 0;
+
+        public void showTemp()
+        {
+
+            if (temperature == 0)
+            {
+                throw (new IsZeroException("Zero Temperature exception"));
+            }
+            else
+            {
+                Console.WriteLine("Temperature: {0}", temperature);
+            }
+        }
+    }
+}
+
